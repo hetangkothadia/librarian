a65cf45e14670d5f6691f31c4503fa80ee0e0a03
Hetang <hetang.kothadia@gmail.com>
2023-03-17 14:34:04 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <navbar></navbar>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+import Navbar from './components/Navbar.vue';
+ 
+export default {
+  name: 'App',
+  
+  data: () => ({
+    //
+  }),
+  components: {Navbar},
+};
+</script>
+
+<style>
+@import url('@/assets/style.css');
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="center">
+        <v-row>
+            <v-col cols="10" offset="1" sm="6" offset-sm="3" md="4" offset-md="4">
+                <v-avatar color="primary" rounded size="56" tile>
+                    <v-img src="@/assets/logo.png"></v-img>
+                </v-avatar>
+                <v-text-field label="Username" v-model="username"></v-text-field>
+                <v-text-field label="Password" type="password" v-model="password"></v-text-field>
+                <v-btn class="base-inverted" block @click="authenticate" >
+                    Login
+                </v-btn>
+            </v-col>
+        </v-row>
+    </div>
+</template>
+
+<script>
+export default{
+  name: "Login",
+
+  data(){
+    return{
+        username: "",
+        password: "",
+    }
+  },
+  methods: {
+    authenticate() {
+        alert("Login Clicked");
+    }
+  },
+}
+
+</script>
+
+<style></style>
